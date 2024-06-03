01adf5f99a791e6e51c9cad7b886b8f674854178
Jean Carlos Inferdes <104872644+Inferdes@users.noreply.github.com>
2024-06-03 11:42:34 -0300

    aula 3 parcial

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-resultado");
