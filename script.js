4d2b163157c36f0672df264ce3072d12e4c9c825
Jean Carlos Inferdes <104872644+Inferdes@users.noreply.github.com>
2024-06-10 11:17:02 -0300

    aula 3

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-Alternativas");
 const caixaResultado = document.querySelector(".caixa-Resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+    enunciado: "Pergunta 1",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 2",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+]
